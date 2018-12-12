ebdd80d801c0bc5f62c75e6a7c888f83da692140
laoqixin <lmao6688@gmail.com>
2018-12-12 18:58:15 +0800

    continue...

---
+++ src/main/java/com/laoqixin/service/EmployeeService.java
@@ -0,0 +1,69 @@
+package com.laoqixin.service;
+
+import com.laoqixin.bean.Employee;
+import com.laoqixin.dao.EmployeeMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/*
+ *  @项目名：  人员管理系统0.1
+ *  @包名：    com.laoqixin.service
+ *  @文件名:   EmployeeService
+ *  @创建者:   laoqixin
+ *  @创建时间:  2018/12/11 21:38
+ *  @描述：    编写增删改查的业务层 实现dao中的接口方法
+ */
+@Service
+public class EmployeeService {
+    private static final String TAG = "EmployeeService";
+    @Autowired
+    EmployeeMapper mEmployeeMapper;
+
+    /**
+     * 查询所有员工（分页查询）
+     * @return
+     * ma
+     */
+    public List<Employee> selectAll() {
+        return mEmployeeMapper.selectAll();
+    }
+    /**
+     * 根据id查询员工信息
+     * @param empId
+     * @return
+     */
+    public Employee selectByPrimaryKey(Integer empId)
+    {
+        return mEmployeeMapper.selectByPrimaryKey(empId);
+    }
+
+    /**
+     * 根据id删除员工信息
+     * @param empId
+     */
+    public void deleteByPrimaryKey(Integer empId)
+    {
+        mEmployeeMapper.deleteByPrimaryKey(empId);
+    }
+    /**
+     * 修改员工信息
+     * @param record
+     */
+    public void  updateByPrimaryKey(Employee record)
+    {
+        mEmployeeMapper.updateByPrimaryKey(record);
+    }
+
+    /**
+     * 增加员工信息
+     * @param record
+     */
+    public void  insert(Employee record)
+    {
+        mEmployeeMapper.insert(record);
+    }
+
+}
---
+++ src/main/java/com/laoqixin/service/UserService.java
@@ -0,0 +1,19 @@
+package com.laoqixin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ *  @项目名：  人员管理系统0.1
+ *  @包名：    com.laoqixin.service
+ *  @文件名:   UserService
+ *  @创建者:   laoqixin
+ *  @创建时间:  2018/12/12 10:56
+ *  @描述：    TODO
+ */
+@Service
+public class UserService {
+    @Autowired
+    UserService mUserService;
+
+}
